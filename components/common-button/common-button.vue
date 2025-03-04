5def37cac480ce5c6a672560b36c85057567b44d
pjzhai <pjzhai@outlook.com>
2025-03-04 20:26:23 +0800

    1

---
+++ components/common-button/common-button.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="root">
+		<view class="left">
+			<uni-icons type="download-filled" color="green" size="50rpx"></uni-icons>
+			<view class="title">我的下载</view>
+		</view>
+		<view class="right">
+			<view class="num">11</view>
+			<uni-icons type="right" color="green" size="50rpx"></uni-icons>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.root{
+	display: flex;
+	align-items: center;
+	background-color: #EEE;
+	height: 120rpx;
+	justify-content: space-between;
+	border-radius: 10rpx;
+	overflow: hidden;
+	box-shadow: 0 0 50rpx rgba(0, 0, 0, 0.04);
+	.right{
+		display: flex;
+		align-items: center;
+		margin-right: 20rpx;
+	}
+	.left{
+		display: flex;
+		align-items: center;
+		margin-left: 20rpx;
+		.title{
+			font-size: 35rpx;
+			margin-left: 15rpx;
+		}
+	}
+}
+</style>
--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,7 +1,12 @@
 <template>
-  <view>
-    
+  <view class="header">
+		<image class="img-avatar" src="/static/xxmLogo.png"></image>
+		<view class="name">孤烟</view>
+		<view class="address">来自于：山西</view>
   </view>
+	<view class="nav1">
+		<common-button></common-button>
+	</view>
 </template>
 
 <script setup>
@@ -9,5 +14,27 @@
 </script>
 
 <style lang="scss" scoped>
-         
+.header{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 450rpx;
+	.img-avatar{
+		width: 200rpx;
+		height: 200rpx;
+		border-radius: 50%;
+		margin-bottom: 20rpx;
+	}
+	.name{
+		font-size: 50rpx;
+		margin-bottom: 5rpx;
+	}
+	.address{
+		color: #333;
+	}
+} 
+.nav1{
+	margin: 0 30rpx;
+}
 </style>
