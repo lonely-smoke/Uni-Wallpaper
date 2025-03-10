06a94893ef5c1a4ae488eddadbd34bd36adf6d73
pjzhai <pjzhai@outlook.com>
2025-03-10 17:29:40 +0800

    4

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,7 +1,9 @@
 <template>
   <view class="list">
-    <view class="imgContainer">
-      <image class="img" src="/common/images/preview1.jpg"></image>
+    <view class="imgContainer" v-for="index in 10">
+      <navigator url="/pages/preview/preview">
+		  <image class="img" mode="widthFix" src="/common/images/preview1.jpg"></image>
+	  </navigator>
     </view>
   </view>
 </template>
@@ -9,9 +11,16 @@
 <script setup>
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .list{
+    // padding: 15rpx;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
+	gap: 5rpx;
+	.img{
+		width: 100%;
+		height: fit-content;
+		opacity: 0.1;
+	}
   }
 </style>
---
+++ pages/preview/preview.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="preview">
+		<image class="img" src="/common/images/preview2.jpg"></image>
+		<view class="mask"></view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.preview{
+	.mask
+}
+</style>
